4e7d01cfca1a3c351588098222159f343b3e5958
SHIVRAJ RK <shivrajrk03@gmail.com>
2025-05-05 00:20:34 +0530

    pic

--- backend/src/controllers/message_controller.js
+++ backend/src/controllers/message_controller.js
@@ -37,7 +37,7 @@ export const getMessages = async (req, res) => {
 
 export const sendMessage = async (req, res) => {
   try {
-    const { text, image } = req.body;
+    const { text, image } = req.body || {};
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
